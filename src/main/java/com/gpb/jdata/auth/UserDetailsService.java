8bc8d4349489527c6d89c9451a9838ce682ceeb8
Vladimir Pak <pvolodyas@gmail.com>
2025-11-10 15:46:38 +0300

    Добавлена базовая аутентификация

---
+++ src/main/java/com/gpb/jdata/auth/UserDetailsService.java
@@ -0,0 +1,28 @@
+package com.gpb.jdata.auth;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.gpb.jdata.models.UserEntity;
+import com.gpb.jdata.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles("USER")
+                .disabled(!user.isEnabled())
+                .build();
+    }
+}
