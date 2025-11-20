3b75b2fb8c235da6448b55d03c95be1b9b229c30
Vladimir Pak <pvolodyas@gmail.com>
2025-11-20 11:29:07 +0300

    Исправлено authSuccess log

--- src/main/java/com/gpb/jdata/log/SvoiCustomLogger.java
+++ src/main/java/com/gpb/jdata/log/SvoiCustomLogger.java
@@ -209,7 +209,7 @@ public class SvoiCustomLogger {
                     ip
             );
 
-            send("authSuccess", "Invalid Login or Password", message,
+            send("authSuccess", "Authentication success", message,
                     SvoiSeverityEnum.FIVE, journal);
 
         } catch (Exception ex) {
