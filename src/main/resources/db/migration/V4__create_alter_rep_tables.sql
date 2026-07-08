02778715f623ce227fbcc36dc12439cc78ec7ea0
Vladimir Pak <pvolodyas@gmail.com>
2026-07-08 22:22:21 +0300

    fixed migrations

--- src/main/resources/db/migration/V4__create_alter_rep_tables.sql
+++ src/main/resources/db/migration/V4__create_alter_rep_tables.sql
@@ -1,3 +1,10 @@
+CREATE TABLE IF NOT EXISTS jdata.pg_attrdef_rep_tmp (
+	adrelid int8 NOT NULL,
+	adnum int8 NOT NULL,
+	adbin varchar(255) NULL,
+	CONSTRAINT pg_attrdef_rep_tmp_pkey PRIMARY KEY (adrelid, adnum)
+);
+
 DROP TABLE IF EXISTS jdata.pg_partition_rule_rep;
 
 CREATE TABLE IF NOT EXISTS jdata.pg_partition_rule_rep (
--- src/main/resources/db/migration/V4__create_temp_attrdef.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS jdata.pg_attrdef_rep_tmp (
-	adrelid int8 NOT NULL,
-	adnum int8 NOT NULL,
-	adbin varchar(255) NULL,
-	CONSTRAINT pg_attrdef_rep_tmp_pkey PRIMARY KEY (adrelid, adnum)
-);
